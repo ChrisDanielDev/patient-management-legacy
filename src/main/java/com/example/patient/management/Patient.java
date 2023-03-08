c68d1d6e2b5ae7e3b41e4fedad6ff7a08369f0c3
Chris <chris@email.com>
2023-03-08 13:39:46 +0200

    feat: add ID attriubute

--- src/main/java/com/example/patient/management/Patient.java
+++ src/main/java/com/example/patient/management/Patient.java
@@ -1,7 +1,10 @@
 package com.example.patient.management;
+import java.util.Objects;
+import java.util.UUID;
 
 public class Patient {
 
+    private final UUID ID;
     private PatientFullName _fullName;
     private Address _address;
 
@@ -13,7 +16,8 @@ public class Patient {
         return _address;
     }
 
-    private Patient(PatientFullName patientFullName, Address address) {
+    private Patient(UUID ID, PatientFullName patientFullName, Address address) {
+        this.ID = ID;
         this._fullName = patientFullName;
         this._address = address;
     }
@@ -44,7 +48,20 @@ public class Patient {
         Result<Address> addressResult  = Address.create(streetName, CountryName, StateName, City);
         Result<PatientFullName> fullNameResult  = PatientFullName.create(FirstName, LastName);
 
-        return Result.success(new Patient(fullNameResult.getValue(), addressResult.getValue()));
+        UUID ID = UUID.randomUUID();
+
+        return Result.success(new Patient(ID, fullNameResult.getValue(), addressResult.getValue()));
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Patient patient = (Patient) obj;
+        return Objects.equals(ID, patient.ID);
+    }
 }
